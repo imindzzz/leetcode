458432788e01adbb26833128eb54fe4e255f6c1a
fusun <332553703@qq.com>
2018-06-19 14:40:37 +0800

    [5] Longest Palindromic Substring  Manacher 失败

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -33,28 +33,30 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
-    let maxObj = { max: -Infinity, i: NaN, j: NaN }
-    for (let i = 0; i < s.length; i++) {
-        for (let j = s.length - 1; j >= i; j--) {
-            if (isPalindrome(s, i, j)) {
-                if (maxObj.max < (j - i + 1)) {
-                    maxObj = {
-                        max: (j - i + 1),
-                        i, j
-                    }
-                }
-            }
+    let st = "$" + s.split("").join("#");
+    let Len = [0];
+    let mx = 0, ans = 0, po = 0;//mx即为当前计算回文串最右边字符的最大值
+    for (let i = 1; i <= st.length; i++) {
+        if (mx > i) {
+            Len[i] = Math.min(mx - i, Len[2 * po - i]);//在Len[j]和mx-i中取个小
+        } else {
+            Len[i] = 1;//如果i>=mx，要从头开始匹配
+        }
+        while (st[i - Len[i]] == st[i + Len[i]]) {
+            Len[i]++;
+        }
+        if (Len[i] + i > mx) {//若新计算的回文串右端点位置大于mx，要更新po和mx的值
+            mx = Len[i] + i;
+            po = i;
+        }
+        // ans = Math.max(ans, Len[i]);
+        if (Len[ans] < Len[i]) {
+            ans = i;
         }
     }
-    return s.substr(maxObj.i, maxObj.max);
+    // console.log(st.split(""));
+    // console.log(Len);
+    let re = st.substr(ans - Len[ans], ans + Len[ans]);
+    re = re.match(/[a-z]/ig).join("");
+    return re;//返回Len[i]中的最大值-1即为原串的最长回文子串额长度 
 };
-var isPalindrome = function (s, i, j) {
-    while (i < j) {
-        if (s[i] != s[j]) {
-            return false
-        }
-        i++;
-        j--;
-    }
-    return true;
-}
