5ef6acbca5043aab26e952c92136a1fa72db6ac8
xd199153 <332553703@qq.com>
2019-08-12 21:17:55 +0800

    [1] Two Sum

---
+++ 1.two-sum.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int> temp = {};
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                // cout << nums[i] << nums[j] << target << '\n';
+                if (nums[i] + nums[j] == target)
+                {
+                    temp.push_back(i);
+                    temp.push_back(j);
+                    // cout << i;
+                    break;
+                }
+            }
+        }
+        return temp;
+    }
+};
+
+// int main()
+// {
+//     Solution sol;
+//     vector<int> nums = {2, 7, 11, 15};
+//     sol.twoSum(nums, 9);
+// }
