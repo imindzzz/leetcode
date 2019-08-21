289aef180beaf7a1016f365565b4a515d63732cd
xd199153 <332553703@qq.com>
2019-08-21 15:20:28 +0800

    description

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -2,6 +2,32 @@
  * @lc app=leetcode id=1 lang=cpp
  *
  * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (44.40%)
+ * Likes:    11493
+ * Dislikes: 396
+ * Total Accepted:    2M
+ * Total Submissions: 4.5M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
  */
 #include <bits/stdc++.h>
 #include <vector>
